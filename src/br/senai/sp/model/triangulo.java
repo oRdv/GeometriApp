95a93f57366ba95e9ff9f58bc6cb3a07c4a63573
oR <tamiressenaisp@gmail.com>
2023-03-01 16:47:30 -0300

    geometria app

---
+++ src/br/senai/sp/model/triangulo.java
@@ -0,0 +1,22 @@
+package br.senai.sp.model;
+
+public class triangulo {
+
+	public double base;
+	public double ladoB;
+	public double ladoC;
+	public double altura;
+	public String nome;
+	
+	public void exibirDados() {
+		System.out.println("----------------");	
+		System.out.println(nome);	
+		System.out.println("----------------");	
+		System.out.println("Base: " + base);	
+		System.out.println("Altura: " + altura);	
+		System.out.println("LadoB: " + ladoB);	
+		System.out.println("LadoC: " + ladoC);		
+		
+	}
+	
+}
